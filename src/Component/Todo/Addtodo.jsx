6086e3248d952747b87ba92babfb9576e78655d9
Shubham Tribhuvan <shubhamtribhuvan8@gmail.com>
2023-07-15 11:00:33 +0530

    Added table for task

--- src/Component/Todo/Addtodo.jsx
+++ src/Component/Todo/Addtodo.jsx
@@ -1,7 +1,16 @@
 import React, { useContext } from "react";
-import { Input } from "@mui/material";
-import { Button } from "react-bootstrap";
+import { TextField, Button } from "@mui/material";
 import { TodoContext } from "./TodoProvider";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+} from "@mui/material";
+import { Label } from "@mui/icons-material";
 
 const AddTodo = () => {
   const {
@@ -16,35 +25,89 @@ const AddTodo = () => {
 
   return (
     <div>
-      <h2>To-Do List</h2>
-      <Input
+      <h2>To-Do LIST</h2>
+      <TextField
         type="text"
         value={newTask}
         onChange={(e) => setNewTask(e.target.value)}
         placeholder="Enter a new task"
-      />
-      <Button onClick={addTask}>Add Task</Button>
-      <p>Total Tasks: {tasks.length}</p>
-      <p>Completed Tasks: {completedTasksCount}</p>
-      <ul>
-        {tasks.map((task) => (
-          <li key={task.id}>
-            <input
-              type="checkbox"
-              checked={task.completed}
-              onChange={() => toggleTask(task.id)}
-            />
-            <span
-              style={{
-                textDecoration: task.completed ? "line-through" : "none",
-              }}
-            >
-              {task.text}
-            </span>
-            <Button onClick={() => removeTask(task.id)}>Remove</Button>
-          </li>
-        ))}
-      </ul>
+        required
+        size="small"
+      />{" "}
+      <Button variant="contained" onClick={addTask} size="large">
+        Add Task
+      </Button>
+      <br />
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <Label>Task Table</Label>
+        <TableContainer component={Paper}>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>Sr.No</TableCell>
+                <TableCell>Added Task</TableCell>
+                <TableCell>Remove Task</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {tasks.map((task, index) => (
+                <TableRow key={task.id}>
+                  <TableCell>{index + 1}</TableCell>
+                  <TableCell>
+                    <input
+                      type="checkbox"
+                      checked={task.completed}
+                      onChange={() => toggleTask(task.id)}
+                    />
+                    <span
+                      style={{
+                        textDecoration: task.completed
+                          ? "line-through"
+                          : "none",
+                      }}
+                    >
+                      {task.text}
+                    </span>
+                  </TableCell>
+                  <TableCell>
+                    <Button
+                      onClick={() => removeTask(task.id)}
+                      variant="outlined"
+                      size="small"
+                    >
+                      Remove
+                    </Button>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </div>
+      <div
+        style={{ display: "flex", justifyContent: "center", marginTop: "16px" }}
+      >
+        <TableContainer component={Paper}>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>Title</TableCell>
+                <TableCell>Count</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              <TableRow>
+                <TableCell>Total Tasks</TableCell>
+                <TableCell>{tasks.length}</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>Completed Tasks</TableCell>
+                <TableCell>{completedTasksCount}</TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </div>
     </div>
   );
 };
